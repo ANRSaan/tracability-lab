065cf69d61922ed0568e91e67548d7fd41a341c6
Brian Williams <heffergod@gmail.com>
2022-09-22 14:54:50 -0700

    attempting to fix endpoints

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get('/api/clicky', (req, res) => {
+app.get('http://localhost:5050/api/clicky', (req, res) => {
     res.status(200).send()
 })
 
